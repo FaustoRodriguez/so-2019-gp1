b7e73f3829fbf871045a137b37b27b0715e3e2ad
FaustoRodriguez <A01634943@ITESM.MX>
2019-10-15 00:51:31 +0000

    Sleep

---
+++ xv6-public/sleep.c
@@ -0,0 +1,19 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char **argv)
+{
+  int i;
+  if(argc < 2){
+    printf(2, "usage: sleep centSeconds...\n");
+    exit();
+  }
+  for(i=1; i<argc; i++)
+  	if(atoi(argv[i])>=0)
+    	sleep(atoi(argv[i]));
+	else
+		printf(2, "Negative sleep time not valid\n");
+  exit();
+}
